ba51607632a5c52145ae326bcce6d431d508c88e
Mariia <ma.gaiduchenko@gmail.com>
2022-07-13 00:00:44 +0300

    added redux store element

---
+++ src/components/app/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { userSlice } from '../../features/UserSlice';
+export default configureStore({
+  reducer: {
+    user: userSlice.reducer,
+  },
+});
