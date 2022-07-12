9681f20c2c741341909d0ad1a0d1c2f831a300d6
Mariia <ma.gaiduchenko@gmail.com>
2022-07-13 00:01:27 +0300

    added UserSlice element

---
+++ src/features/UserSlice.js
@@ -0,0 +1,172 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const signupUser = createAsyncThunk(
+  "users/signupUser",
+  async ({ fullName, email, password }, thunkAPI) => {
+    try {
+      const response = await fetch(
+        "https://travel-app-api.glitch.me/api/v1/auth/sign-up",
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            fullName: fullName,
+            email: email,
+            password: password,
+          }),
+        }
+      );
+      let data = await response.json();
+      console.log("data", data);
+
+      if (response.status === 200) {
+        localStorage.setItem("token", data.token);
+        return { ...data, fullName: fullName, email: email };
+      } else {
+        return thunkAPI.rejectWithValue(data);
+      }
+    } catch (e) {
+      console.log("Error", e.response.data);
+      return thunkAPI.rejectWithValue(e.response.data);
+    }
+  }
+);
+
+export const loginUser = createAsyncThunk(
+  "users/loginUser",
+  async ({ email, password }, thunkAPI) => {
+    try {
+      const response = await fetch(
+        "https://travel-app-api.glitch.me/api/v1/auth/sign-in",
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          }),
+        }
+      );
+      let data = await response.json();
+      console.log("response", data);
+      if (response.status === 200) {
+        localStorage.setItem("token", data.token);
+        return data;
+      } else {
+        return thunkAPI.rejectWithValue(data);
+      }
+    } catch (e) {
+      console.log("Error", e.response.data);
+      thunkAPI.rejectWithValue(e.response.data);
+    }
+  }
+);
+
+export const fetchUserBytoken = createAsyncThunk(
+  "users/fetchUserByToken",
+  async ({ token }, thunkAPI) => {
+    try {
+      const response = await fetch(
+        "https://travel-app-api.glitch.me/api/v1/auth/authenticated-user",
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            Authorization: token,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      let data = await response.json();
+      console.log("data", data, response.status);
+
+      if (response.status === 200) {
+        return { ...data };
+      } else {
+        return thunkAPI.rejectWithValue(data);
+      }
+    } catch (e) {
+      console.log("Error", e.response.data);
+      return thunkAPI.rejectWithValue(e.response.data);
+    }
+  }
+);
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    username: "",
+    email: "",
+    isFetching: false,
+    isSuccess: false,
+    isError: false,
+    errorMessage: "",
+  },
+  reducers: {
+    clearState: (state) => {
+      state.isError = false;
+      state.isSuccess = false;
+      state.isFetching = false;
+
+      return state;
+    },
+  },
+  extraReducers: {
+    [signupUser.fulfilled]: (state, { payload }) => {
+      console.log("payload", payload);
+      state.isFetching = false;
+      state.isSuccess = true;
+      state.email = payload.user.email;
+      state.username = payload.user.username;
+    },
+    [signupUser.pending]: (state) => {
+      state.isFetching = true;
+    },
+    [signupUser.rejected]: (state, { payload }) => {
+      state.isFetching = false;
+      state.isError = true;
+      state.errorMessage = payload.message;
+    },
+    [loginUser.fulfilled]: (state, { payload }) => {
+      state.email = payload.email;
+      state.username = payload.username;
+      state.isFetching = false;
+      state.isSuccess = true;
+      return state;
+    },
+    [loginUser.rejected]: (state, { payload }) => {
+      console.log("payload", payload);
+      state.isFetching = false;
+      state.isError = true;
+      state.errorMessage = payload.message;
+    },
+    [loginUser.pending]: (state) => {
+      state.isFetching = true;
+    },
+    [fetchUserBytoken.pending]: (state) => {
+      state.isFetching = true;
+    },
+    [fetchUserBytoken.fulfilled]: (state, { payload }) => {
+      state.isFetching = false;
+      state.isSuccess = true;
+
+      state.email = payload.email;
+      state.username = payload.username;
+    },
+    [fetchUserBytoken.rejected]: (state) => {
+      console.log("fetchUserBytoken");
+      state.isFetching = false;
+      state.isError = true;
+    },
+  },
+});
+
+export const { clearState } = userSlice.actions;
+
+export const userSelector = (state) => state.user;
