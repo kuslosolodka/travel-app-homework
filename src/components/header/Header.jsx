609f066f56c268b68af009829a18dc3b278e9d14
Mariia <ma.gaiduchenko@gmail.com>
2022-07-14 00:39:43 +0300

    added logout feature

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,8 +2,18 @@ import React from "react";
 import Briefcase from '../../assets/images/briefcase.svg';
 import User from '../../assets/images/user.svg';
 import '../../assets/css/style.css';
+import { userSelector } from "../../features/UserSlice";
 
-const Header = () => (
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { clearState } from "../../features/UserSlice";
+import { logout } from "../../features/UserSlice";
+function Header () {
+  // const {userInfo, userToken, isSuccess, isError, errorMessage } = useSelector(userSelector);
+  const dispatch = useDispatch();
+  const { userName} = useSelector(userSelector);
+  return(
   <header className="header">
     <div className="header__inner">
       <a href="/main" className="header__logo">
@@ -22,13 +32,13 @@ const Header = () => (
               <span className="visually-hidden">Profile</span>
               <img src={User} alt="profile icon" />
               <ul className="profile-nav__list">
-                <li className="profile-nav__item profile-nav__username">
-                  John Doe
+                <li className="profile-nav__item profile-nav__username">{userName}
                 </li>
                 <li className="profile-nav__item">
-                  <button className="profile-nav__sign-out button">
+                  <button className="profile-nav__sign-out button" onClick={()=> dispatch(logout())}>
                     Sign Out
                   </button>
+              
                 </li>
               </ul>
             </div>
@@ -37,6 +47,7 @@ const Header = () => (
       </nav>
     </div>
   </header>
-);
+  )
+};
 
 export default Header;
