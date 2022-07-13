7f12af4df9955a11a884c9cbf7d321fb1af2da16
Mariia <ma.gaiduchenko@gmail.com>
2022-07-14 00:35:04 +0300

    added trips and bookings reducers

--- src/components/app/store.js
+++ src/components/app/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { userSlice } from '../../features/UserSlice';
+import tripReducer from '../../features/TripSlice';
+import bookingReducer from "../../features/BookingsSlice"
+
 export default configureStore({
   reducer: {
     user: userSlice.reducer,
+    trip: tripReducer,
+    booking: bookingReducer
   },
 });
