88574dafb97e333bb9dbe1a079c54a469956a6b4
Mariia <ma.gaiduchenko@gmail.com>
2022-07-14 00:39:15 +0300

    fetched api json to jsx

--- src/components/bookings-page/BookingsPage.jsx
+++ src/components/bookings-page/BookingsPage.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import bookingslist from '../../local-json/bookinglist.json';
 import '../../assets/css/style.css';
-
-function BookingsPage () { 
-  const [foundBookings] = React.useState(bookingslist);
-return(
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getAllBookings } from '../../features/BookingsSlice';
+export const BookingsPage = () => { 
+  const booking = useSelector(state => state.booking);
+    const dispatch = useDispatch();
+    useEffect(()=>{
+      dispatch(getAllBookings())
+    }, [])
+  // const [foundBookings] = React.useState(bookingslist);
+return (
 <main className="bookings-page">
       <h1 className="visually-hidden">Travel App</h1>
-     {[...foundBookings]
-     .sort((a,b) => new Date(a.date).getTime - new Date(b.date).getTime())
-     .map(booking => {
-       return(
-         <ul className="booking__list">
+     {/* {[...foundBookings] */}
+     {/* .sort((a,b) => new Date(a.date).getTime - new Date(b.date).getTime())
+     .map(booking => { */}
+      {booking.loading && <div>Loading...</div>}
+      {!booking.loading && booking.error ? <div>Error: {booking.error}</div> : null}
+      {!booking.loading && booking.bookings.length ? (
+      <ul className="booking__list">
+      {booking.bookings.map(booking=> (
            <li className="booking">
              <h3 className="booking__title">Country</h3>
              <span className="booking__guests">{booking.guests} guests</span>
@@ -22,12 +33,11 @@ return(
             ×
           </button>
            </li>
+           ))}
          </ul>
-       );
-     })
-}
+      ) : null}
     </main>
-);
+ );
 }
 
 export default BookingsPage;
