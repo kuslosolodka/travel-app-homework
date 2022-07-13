208aa9509068ce9f6a0340220c4d592238065113
Mariia <ma.gaiduchenko@gmail.com>
2022-07-14 00:36:00 +0300

    added local loader

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -50,7 +50,7 @@ function SignIn() {
     }
   }, [isError, isSuccess]);
 
-  console.log(errors);
+  
   return (
     <>
       <main className="sign-in-page">
@@ -91,26 +91,7 @@ function SignIn() {
           </label>
           <button className="button" type="submit">
             {isFetching ? (
-              <svg
-                class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  class="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  stroke-width="4"
-                ></circle>
-                <path
-                  class="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                ></path>
-              </svg>
+              <div className="loader"></div>
             ) : null}
             Sign in
           </button>
