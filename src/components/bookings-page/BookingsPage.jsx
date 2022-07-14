3884cb9672f493f1599ff21e1e0b8aaf02563fef
Mariia <ma.gaiduchenko@gmail.com>
2022-07-14 03:22:14 +0300

    fetched api json to booking page

--- src/components/bookings-page/BookingsPage.jsx
+++ src/components/bookings-page/BookingsPage.jsx
@@ -1,44 +1,97 @@
-import React from 'react';
-import bookingslist from '../../local-json/bookinglist.json';
-import '../../assets/css/style.css';
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { getAllBookings } from '../../features/BookingsSlice';
-export const BookingsPage = () => { 
-  const booking = useSelector(state => state.booking);
-    const dispatch = useDispatch();
-    useEffect(()=>{
-      dispatch(getAllBookings())
-    }, [])
-  // const [foundBookings] = React.useState(bookingslist);
-return (
-<main className="bookings-page">
+import React from "react";
+import "../../assets/css/style.css";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getAllBookings } from "../../features/BookingsSlice";
+import { bookingSelector } from "../../features/BookingsSlice";
+import { clearState } from "../../features/BookingsSlice";
+import { useNavigate } from "react-router-dom";
+import { tripSelector } from "../../features/TripSlice";
+
+export const BookingsPage = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { isFetching, isError } = useSelector(tripSelector);
+
+  useEffect(() => {
+    dispatch(getAllBookings({ token: localStorage.getItem("token") }));
+  }, []);
+  
+  const { guests, date, totalPrice } = useSelector(bookingSelector);
+  
+  useEffect(() => {
+    if (isError) {
+      dispatch(clearState());
+      navigate("/sign-in");
+    }
+  }, [isError]);
+  
+  return (
+    <main className="bookings-page">
       <h1 className="visually-hidden">Travel App</h1>
-     {/* {[...foundBookings] */}
-     {/* .sort((a,b) => new Date(a.date).getTime - new Date(b.date).getTime())
-     .map(booking => { */}
-      {booking.loading && <div>Loading...</div>}
-      {!booking.loading && booking.error ? <div>Error: {booking.error}</div> : null}
-      {!booking.loading && booking.bookings.length ? (
-      <ul className="booking__list">
-      {booking.bookings.map(booking=> (
-           <li className="booking">
-             <h3 className="booking__title">Country</h3>
-             <span className="booking__guests">{booking.guests} guests</span>
-             <span className="booking__date">{(booking.date)}</span>
-             <span className="booking__total">{booking.totalPrice} $</span>
-             <button className="booking__cancel" title="Cancel booking">
-            <span className="visually-hidden">Cancel booking</span>
-            ×
-          </button>
-           </li>
-           ))}
-         </ul>
-      ) : null}
+      {isFetching ? (
+        <div className="loader"></div>
+      ) : (
+        <ul className="booking__list">
+          <li className="booking">
+            <h3 className="booking__title">Country</h3>
+            <span className="booking__guests">{guests} guests</span>
+            <span className="booking__date">{date}</span>
+            <span className="booking__total">{totalPrice} $</span>
+            <button className="booking__cancel" title="Cancel booking">
+              <span className="visually-hidden">Cancel booking</span>×
+            </button>
+          </li>
+        </ul>
+      )}
+      {isFetching ? (
+        <div className="loader"></div>
+      ) : (
+        <ul className="booking__list">
+          <li className="booking">
+            <h3 className="booking__title">Country</h3>
+            <span className="booking__guests">{guests} guests</span>
+            <span className="booking__date">{date}</span>
+            <span className="booking__total">{totalPrice} $</span>
+            <button className="booking__cancel" title="Cancel booking">
+              <span className="visually-hidden">Cancel booking</span>×
+            </button>
+          </li>
+        </ul>
+      )}
+      {isFetching ? (
+        <div className="loader"></div>
+      ) : (
+        <ul className="booking__list">
+          <li className="booking">
+            <h3 className="booking__title">Country</h3>
+            <span className="booking__guests">{guests} guests</span>
+            <span className="booking__date">{date}</span>
+            <span className="booking__total">{totalPrice} $</span>
+            <button className="booking__cancel" title="Cancel booking">
+              <span className="visually-hidden">Cancel booking</span>×
+            </button>
+          </li>
+        </ul>
+      )}
+      {isFetching ? (
+        <div className="loader"></div>
+      ) : (
+        <ul className="booking__list">
+          <li className="booking">
+            <h3 className="booking__title">Country</h3>
+            <span className="booking__guests">{guests} guests</span>
+            <span className="booking__date">{date}</span>
+            <span className="booking__total">{totalPrice} $</span>
+            <button className="booking__cancel" title="Cancel booking">
+              <span className="visually-hidden">Cancel booking</span>×
+            </button>
+          </li>
+        </ul>
+      )}
     </main>
- );
-}
+  );
+};
 
 export default BookingsPage;
-
